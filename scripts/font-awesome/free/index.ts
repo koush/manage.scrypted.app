c05b17a0f8874208e2ea602c0637be6bb3e17bcb
Koushik Dutta <koushd@gmail.com>
2024-07-07 12:20:30 -0700

    missing files

---
+++ scripts/font-awesome/free/index.ts
@@ -0,0 +1,6 @@
+import { setFaPrefix } from '../../../src/common/fa-prefix'
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
+import './solid.css'
+import './brands.css'
+
+setFaPrefix('fa-solid');
---
+++ scripts/font-awesome/pro/index.ts
@@ -0,0 +1,7 @@
+import { setFaPrefix } from '../../../src/common/fa-prefix'
+import '@fortawesome/fontawesome-pro/css/fontawesome.min.css'
+import './light.css'
+import './solid.css'
+import './brands.css'
+
+setFaPrefix('fa-light');
