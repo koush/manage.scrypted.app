6296fdaff26020e5f0f4510a8c76bf7296a53552
Koushik Dutta <koushd@gmail.com>
2024-07-18 11:51:00 -0700

    legacy ui unregister

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+// unregister legacy core plugin ui service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.registration.unregister().then(() => {
+      return self.clients.matchAll();
+    }).then(clients => {
+      clients.forEach(client => client.navigate(client.url));
+    })
+  );
+});
