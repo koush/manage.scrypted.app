636af7fbd92d8ee9d95e2d35bd180e68cecbf4e3
Koushik Dutta <koushd@gmail.com>
2024-07-19 10:41:07 -0700

    add nvr demo link

--- src/components/Launcher.vue
+++ src/components/Launcher.vue
@@ -4,9 +4,6 @@
   }">
     <div>
       <v-card :width="320" theme="light" class="rounded-lg">
-        <!-- <template v-slot:prepend>
-          <v-icon>{{ getFaPrefix('fa-microchip') }} }}</v-icon>
-        </template> -->
         <template v-slot:title>
           <v-card-title style="text-align: center;" class="scrypted-title">Scrypted</v-card-title>
         </template>
@@ -15,7 +12,7 @@
             }}</v-card-subtitle>
           <v-card-subtitle style="text-align: center;" class="scrypted-subtitle2">{{
             connectedClient?.loginResult.hostname
-            }}</v-card-subtitle>
+          }}</v-card-subtitle>
         </template>
         <v-list style="text-align: center;">
           <v-list-item class="pr-16" v-for="item in launcherItems.computed.value"
@@ -46,6 +43,68 @@
           </ToolbarTooltipButton>
         </v-card-actions>
       </v-card>
+
+      <v-card v-if="showNvr" :width="320" theme="dark" class="rounded-lg mt-4">
+        <template v-slot:title>
+          <v-card-title style="text-align: center;" class="scrypted-title">Supercharge Scrypted</v-card-title>
+        </template>
+        <template v-slot:subtitle v-if="connectedClient?.version">
+          <v-card-subtitle style="text-align: center;" class="scrypted-subtitle">Get Scrypted NVR</v-card-subtitle>
+        </template>
+
+        <v-list density="compact">
+          <v-list-item href="https://demo.scrypted.app/#demo" target="_blank">
+            <template v-slot:prepend>
+              <v-icon small>{{ getFaPrefix('fa-timeline') }}</v-icon>
+            </template>
+            <v-list-item-subtitle>
+              24/7 recording with smart detections.
+            </v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item lines="two" href="https://docs.scrypted.app/scrypted-nvr/features.html#rich-notifications" target="_blank">
+            <template v-slot:prepend>
+              <v-icon small>{{ getFaPrefix('fa-video-camera') }}</v-icon>
+            </template>
+            <v-list-item-subtitle>
+              Rich Notifications with a concise thumbnail of the event.
+            </v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item lines="two" href="https://docs.scrypted.app/scrypted-nvr/features.html#adaptive-bitrate" target="_blank">
+            <template v-slot:prepend>
+              <v-icon small>{{ getFaPrefix('fa-bolt-lightning') }}</v-icon>
+            </template>
+            <v-list-item-subtitle>
+              Adaptive bitrate streaming for HomeKit, Google Home, and Alexa.
+            </v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item lines="two" href="https://docs.scrypted.app/scrypted-nvr/apps.html" target="_blank">
+            <template v-slot:prepend>
+              <v-icon small>{{ getFaPrefix('fa-cloud') }}</v-icon>
+            </template>
+            <v-list-item-subtitle>
+              Beautiful iOS, Android, and web app for local and remote viewing.
+            </v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item lines="two" href="https://ha-demo.scrypted.app" target="_blank">
+            <template v-slot:prepend>
+              <v-icon small>{{ getFaPrefix('fa-table-layout') }}</v-icon>
+            </template>
+            <v-list-item-subtitle>
+              Home Assistant cards with snappy streams and two way audio.
+            </v-list-item-subtitle>
+          </v-list-item>
+        </v-list>
+        <div style="width: 100%; display: flex; justify-content: center;" class="mb-2">
+          <v-btn size="small" style="justify-self: center;" href="https://demo.scrypted.app/#/demo" target="_blank">View Demo
+          </v-btn>
+          <v-btn size="small" style="justify-self: center;">Install
+          </v-btn>
+        </div>
+      </v-card>
     </div>
   </div>
 </template>
@@ -58,6 +117,13 @@ import { combineBaseUrl } from '@scrypted/client/src/index';
 import { LauncherApplication, ScryptedInterface } from '@scrypted/types';
 import { deepPurple } from 'vuetify/lib/util/colors.mjs';
 import ToolbarTooltipButton from './ToolbarTooltipButton.vue';
+import { computed } from 'vue';
+
+const showNvr = computed(() => {
+  if (!connectedClient.value)
+    return false;
+  return !connectedClient.value.systemManager.getDeviceById('@scrypted/nvr');
+});
 
 const launcherItems = asyncComputedRaw({
   async get() {
