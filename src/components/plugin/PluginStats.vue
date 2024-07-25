0bfb7a3c8f6a7c9b41baa1e0d9dd7e89b2ca0806
Koushik Dutta <koushd@gmail.com>
2024-07-25 10:53:49 -0700

    graphs

--- src/components/plugin/PluginStats.vue
+++ src/components/plugin/PluginStats.vue
@@ -7,36 +7,55 @@
       <v-card-subtitle class="mt-1">Plugin Stats</v-card-subtitle>
     </template>
 
-    <v-table>
-      <thead>
-        <tr>
-          <th>RPC Objects</th>
-          <th>Pending Results</th>
-          <th>Connections</th>
-        </tr>
-      </thead>
-      <tbody style="font-size: .75rem">
-        <tr v-for="(, i) in chartMax">
-          <td><template v-if="topRpc[i]?.info?.rpcObjects"><span class="bold">{{ topRpc[i]?.info?.rpcObjects
-                }}</span><span class="float-right">{{
-                  topRpc[i]?.name
-                }}</span></template><template v-else>N/A</template></td>
-          <td><template v-if="topPending[i]?.info?.pendingResults"><span class="bold">{{
-            topPending[i]?.info?.pendingResults }}</span><span class="float-right">{{
-                  topPending[i]?.name }}</span></template><template v-else>N/A</template></td>
-          <td><template v-if="topClients[i]?.info?.clientsCount"><span class="bold">{{
-            topClients[i]?.info?.clientsCount }}</span><span class="float-right">{{
-                  topClients[i]?.name }}</span></template><template v-else>N/A</template></td>
-        </tr>
-      </tbody>
-    </v-table>
+    <v-list>
+      <v-list-subheader>RPC Objects</v-list-subheader>
+      <v-list-item v-for="top in topRpc">
+        <v-progress-linear :color="color" :model-value="top?.info?.rpcObjects"
+          :max="max(topRpc.map(t => t.info.rpcObjects))" height="20" rounded>
+          <template v-slot:default>
+            {{ top?.name }}: {{ top?.info?.rpcObjects }}
+          </template>
+        </v-progress-linear>
+      </v-list-item>
+      <v-divider></v-divider>
+
+      <v-list-subheader>Pending Results</v-list-subheader>
+      <v-list-item v-for="top in topPending">
+        <v-progress-linear :color="color" :model-value="top?.info?.pendingResults"
+          :max="max(topRpc.map(t => t.info.pendingResults))" height="20" rounded>
+          <template v-slot:default>
+            {{ top?.name }}: {{ top?.info?.pendingResults }}
+          </template>
+        </v-progress-linear>
+
+      </v-list-item>
+      <v-divider></v-divider>
+
+      <v-list-subheader>Connections</v-list-subheader>
+      <v-list-item v-for="top in topClients">
+        <v-progress-linear :color="color" :model-value="top?.info?.clientsCount"
+          :max="max(topRpc.map(t => t.info.clientsCount))" height="20" rounded>
+          <template v-slot:default>
+            {{ top?.name }}: {{ top?.info?.clientsCount }}
+          </template>
+        </v-progress-linear>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
 <script setup lang="ts">
 import { getFaPrefix } from '@/device-icons';
 import { computed } from 'vue';
 import { PluginModel } from './plugin-common';
+import { max } from 'lodash';
+import { isDark } from '@/common/colors';
+import { chipColor } from '../interfaces/settings-common';
 
+const dark = isDark();
+
+const color = computed(() => {
+  return dark.value ? chipColor : 'light-blue-darken-1';
+});
 
 const props = defineProps<{
   plugins: PluginModel[];
