256b613f25ab6a09217eb6dcfa6be663df83e393
Koushik Dutta <koushd@gmail.com>
2024-07-28 16:22:41 -0700

    missing file

---
+++ src/components/ResponsiveColumn.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-col :class="isTouchPhone ? 'pa-0' : undefined">
+    <slot name="default"></slot>
+  </v-col>
+</template>
+<script setup lang="ts">
+import { isTouchPhone } from '@/common/size';
+</script>
