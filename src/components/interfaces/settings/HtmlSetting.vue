e2a460f2eaa9d3a86c87bd599d909060c9fefe16
Koushik Dutta <koushd@gmail.com>
2024-08-11 13:52:23 -0700

    html cleanup

--- src/components/interfaces/settings/HtmlSetting.vue
+++ src/components/interfaces/settings/HtmlSetting.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
-        <v-list-item-subtitle class="shrink ml-3 mr-3" v-if="modelValue.title">{{ modelValue.title }}</v-list-item-subtitle>
-    <div v-html="modelValue.value"></div>
-</div>
+  <div>
+    <v-divider class="mt-2 mb-2"></v-divider>
+    <v-list-item-subtitle class="shrink ml-3 mr-3 mb-2" v-if="modelValue.title">{{ modelValue.title }}</v-list-item-subtitle>
+    <div style="font-size: .8rem;" v-html="modelValue.value"></div>
+  </div>
 </template>
 <script setup lang="ts">
 import { Setting } from '@scrypted/types';
