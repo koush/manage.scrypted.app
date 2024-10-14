20adc46f59a8cd69a4b32f29d82553e8f38393bd
Brett Jia <dev.bjia56@gmail.com>
2024-10-14 10:59:04 -0400

    plugin-installer: make description optional

--- src/components/plugin/InstallPlugin.vue
+++ src/components/plugin/InstallPlugin.vue
@@ -100,7 +100,7 @@ const plugins = asyncComputed({
     const response = await fetch(url);
     const json = await response.json();
     return json.objects.map((o: any) => ({
-      description: o.package.description.replace(' for Scrypted', ''),
+      description: o.package.description?.replace(' for Scrypted', ''),
       name: o.package.name,
       version: o.package.version,
       username: o.package.publisher.username,
