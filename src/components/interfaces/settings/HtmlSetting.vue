aae465884316f475e3a26863ba551e59ef296bbc
Koushik Dutta <koushd@gmail.com>
2025-02-22 18:11:52 -0800

    html title

--- src/components/interfaces/settings/HtmlSetting.vue
+++ src/components/interfaces/settings/HtmlSetting.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-4">
-    <v-list-item-subtitle class="shrink ml-3 mr-3 mb-2" v-if="modelValue.title">{{ modelValue.title }}</v-list-item-subtitle>
+    <v-list-item-subtitle class="shrink ml-3 mr-3 mb-2" v-if="modelValue.title && !hideTitle">{{ modelValue.title }}</v-list-item-subtitle>
     <div style="font-size: .8rem;" v-html="modelValue.value"></div>
     <v-divider class="mt-2"></v-divider>
   </div>
@@ -9,6 +9,10 @@
 import { Setting } from '@scrypted/types';
 
 const modelValue = defineModel<Setting>();
+
+defineProps<{
+  hideTitle?: boolean;
+}>();
 </script>
 <style scoped>
 .shrink {
