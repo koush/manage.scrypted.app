6fc4ca4ccd68bea5b55ff4243d4e8b5026e0d9cc
Koushik Dutta <koushd@gmail.com>
2025-11-25 22:07:32 -0800

    theme toggle options

--- src/common/components/ThemeToggle.vue
+++ src/common/components/ThemeToggle.vue
@@ -2,25 +2,30 @@
     <v-tooltip text="Toggle Theme" location="top" :open-delay="1000">
         <template v-slot:activator="{ props }">
             <v-fade-transition leave-absolute>
-
-                <v-btn variant="text" @click="toggleTheme" v-bind="props" v-if="theme === 'light'">
-                    <v-icon>{{ getFaPrefix('fa-sun-bright') }}</v-icon>
+                <v-btn :icon="icon" variant="text" @click="toggleTheme" v-bind="props" v-if="theme === 'light'">
+                    <v-icon :size="iconSize">{{ getFaPrefix('fa-sun-bright') }}</v-icon>
                 </v-btn>
-                <v-btn variant="text" @click="toggleTheme" v-bind="props" v-else-if="theme === 'dark'">
-                    <v-icon>{{ getFaPrefix('fa-moon') }}</v-icon>
+                <v-btn :icon="icon" variant="text" @click="toggleTheme" v-bind="props" v-else-if="theme === 'dark'">
+                    <v-icon :size="iconSize">{{ getFaPrefix('fa-moon') }}</v-icon>
                 </v-btn>
-                <v-btn variant="text" @click="toggleTheme" v-bind="props" v-else>
-                    <v-icon>{{ getFaPrefix('fa-moon-over-sun') }}</v-icon>
+                <v-btn :icon="icon" variant="text" @click="toggleTheme" v-bind="props" v-else>
+                    <v-icon :size="iconSize">{{ getFaPrefix('fa-moon-over-sun') }}</v-icon>
                 </v-btn>
             </v-fade-transition>
         </template>
     </v-tooltip>
 </template>
 <script setup lang="ts">
+import type { IconValue } from 'vuetify/lib/composables/icons.mjs';
 import { getFaPrefix } from '../fa-prefix';
 import { getThemeManager } from '../theme';
 
 const tm = getThemeManager();
+
+defineProps<{
+    icon?: boolean | IconValue,
+    iconSize?: string | number,
+}>();
 
 const { theme } = tm;
 function toggleTheme() {
