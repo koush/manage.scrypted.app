0e4160191c3a89593ed66dd011ac6920ff2d2609
Koushik Dutta <koushd@gmail.com>
2025-12-17 09:22:14 -0800

    android check

--- src/common/browser.ts
+++ src/common/browser.ts
@@ -1,6 +1,7 @@
 export const isChrome = navigator.userAgent.includes("Chrome");
 export const isSafari = !isChrome && navigator.userAgent.includes("Safari");
 export const isAppleMobile = navigator.userAgent.includes('iPod') || navigator.userAgent.includes('iPad') || navigator.userAgent.includes('iPhone');
+export const isAndroid = navigator.userAgent.includes('Android');
 export const isInstalledApp = navigator.userAgent.includes('InstalledApp');
 export const supportsOOBLogin = navigator.userAgent.includes('OOBLogin');
 export const isWindows = navigator.userAgent.includes('Windows');
